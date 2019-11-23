83da900c1e9e7acc028ddff9f22a80f1a1807410
Anil Saladi <anil.saladi-ext@ignitionone.com>
2019-11-23 13:41:18 +0530

    Modified the code base with new voltage functionality

--- src/main/java/com/javaworld/awslambda/smarttrack/resource/MPIResourceAPI.java
+++ src/main/java/com/javaworld/awslambda/smarttrack/resource/MPIResourceAPI.java
@@ -1,18 +1,15 @@
 package com.javaworld.awslambda.smarttrack.resource;
 
-import com.javaworld.awslambda.smarttrack.model.SmartTrackRequest;
-import com.javaworld.awslambda.smarttrack.model.TTDPowerSupply;
-import com.javaworld.awslambda.smarttrack.model.Voltage;
+import com.javaworld.awslambda.smarttrack.model.*;
 import com.javaworld.awslambda.smarttrack.serviceImpl.SmartTrackImpl;
 import com.javaworld.awslambda.smarttrack.util.SmartTrackUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.QueryParam;
 import java.util.List;
 
 /**
@@ -21,14 +18,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/message")
 public class MPIResourceAPI {
-    private final Logger logger = LogManager.getLogger(MPIResourceAPI.class.getName());
+    // private final Logger logger = LogManager.getLogger(MPIResourceAPI.class.getName());
 
     @Autowired
     SmartTrackImpl smartTrackImpl;
 
     @RequestMapping(value = "/send", method = RequestMethod.POST, headers = "Accept=application/json")
-    public HttpStatus insertData(@RequestBody List<TTDPowerSupply> ttdPowerSupplyList) {
-       // logger.info("Entered insertData method to dump data into DB with the following data..." + ttdPowerSupply.toString());
+    public HttpStatus insertData(@RequestBody List<DeviceMapping> ttdPowerSupplyList) {
+        // logger.info("Entered insertData method to dump data into DB with the following data..." + ttdPowerSupply.toString());
         boolean isDataInserted = smartTrackImpl.insertData(ttdPowerSupplyList);
         return isDataInserted ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
     }
@@ -47,30 +44,45 @@ public class MPIResourceAPI {
     }*/
 
 
-    @PostMapping(value = "/getVoltage" , consumes = MediaType.APPLICATION_JSON_VALUE,
+    @RequestMapping(value = "/getVoltage" ,method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Voltage> getVoltageData(@RequestBody SmartTrackRequest smartTrackRequest, HttpServletResponse response) {
-        logger.info("Entered getData method for retrieving the requested data...");
+        //  logger.info("Entered getData method for retrieving the requested data...");
+        System.out.println("Entered getData method for retrieving the requested data...");
         try {
             SmartTrackUtils.setResponseHeader(response);
-            List<Voltage> voltageList = smartTrackImpl.getVoltageData(smartTrackRequest.getTimestamp());
-            logger.info("Successfully fetched the requested data...");
+            List<Voltage> voltageList = smartTrackImpl.getVoltageData(smartTrackRequest);
+            System.out.println("Successfully fetched the requested data...");
+            //logger.info("Successfully fetched the requested data...");
             return voltageList;
         } catch (Exception ex) {
-            logger.error("Error occur while fetching TTDPowerSupply details..." + ex);
+            //  logger.error("Error occur while fetching TTDPowerSupply details..." + ex);
+            return null;
+        }
+    }
+
+    @RequestMapping(value = "/getDevices" , consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public DeviceName getDevices(@RequestParam String subStation, HttpServletResponse response) {
+        System.out.println("Entered getData method for retrieving the requested data...");
+        try {
+            SmartTrackUtils.setResponseHeader(response);
+            DeviceName voltageList = smartTrackImpl.getDeviceNames(subStation);
+            return voltageList;
+        } catch (Exception ex) {
             return null;
         }
     }
 
 
-    @RequestMapping(value = "/getDeviceListByName/{deviceName}", method = RequestMethod.GET, headers = "Accept=application/json")
-    public List<TTDPowerSupply> getDeviceDataBasedOnName(@PathVariable String deviceName) {
+    @RequestMapping(value = "/getDeviceListByName", method = RequestMethod.GET, headers = "Accept=application/json")
+    public List<TTDPowerSupply> getDeviceDataBasedOnName(@RequestParam String deviceName) {
         try {
             List<TTDPowerSupply> ttdPowerSupplyList = smartTrackImpl.getDeviceDataWithName(deviceName);
-            logger.info("Successfully fetched the requested data...");
+            // logger.info("Successfully fetched the requested data...");
             return ttdPowerSupplyList;
         } catch (Exception ex) {
-            logger.error("Error occur while fetching TTDPowerSupply details..." + ex);
+            //  logger.error("Error occur while fetching TTDPowerSupply details..." + ex);
             return null;
         }
     }
--- src/main/java/com/javaworld/awslambda/smarttrack/serviceImpl/SmartTrackImpl.java
+++ src/main/java/com/javaworld/awslambda/smarttrack/serviceImpl/SmartTrackImpl.java
@@ -1,5 +1,7 @@
 package com.javaworld.awslambda.smarttrack.serviceImpl;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
@@ -14,14 +16,15 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.javaworld.awslambda.smarttrack.exception.TTDCustomException;
-import com.javaworld.awslambda.smarttrack.model.SmartTrackRequest;
-import com.javaworld.awslambda.smarttrack.model.TTDPowerSupply;
-import com.javaworld.awslambda.smarttrack.model.Voltage;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.javaworld.awslambda.smarttrack.model.*;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
 
 /**
  * Created by anil.saladi on 9/28/2019.
@@ -29,64 +32,65 @@ import java.util.*;
 @SuppressWarnings("deprecation")
 @Service
 public class SmartTrackImpl {
-    private static final Logger logger = LogManager.getLogger(SmartTrackImpl.class.getName());
+    // private static final Logger logger =
+    // LogManager.getLogger(SmartTrackImpl.class.getName());
 
-    public boolean insertData(List<TTDPowerSupply> power) {
+    public boolean insertData(List<DeviceMapping> power) {
         Regions REGION = Regions.US_EAST_1;
         AmazonDynamoDB dynamoDBClient = new AmazonDynamoDBClient();
         dynamoDBClient.setRegion(Region.getRegion(REGION));
         DynamoDBMapper mapper = new DynamoDBMapper(dynamoDBClient);
         try {
-            DynamoDBTableMapper<TTDPowerSupply, String, ?> table = mapper.newTableMapper(TTDPowerSupply.class);
+            DynamoDBTableMapper<DeviceMapping, String, ?> table = mapper.newTableMapper(DeviceMapping.class);
             table.createTableIfNotExists(new ProvisionedThroughput(25L, 25L));
-            logger.info("Connection to the DB started...");
-            logger.info("Connection successfully established...");
+            // logger.info("Connection to the DB started...");
+            // logger.info("Connection successfully established...");
         } catch (Exception ex) {
-            logger.error("DBConnection failed due to " + ex.getMessage());
+            // logger.error("DBConnection failed due to " + ex.getMessage());
         }
         boolean isDataInserted = false;
-        for (TTDPowerSupply ttdPowerSupply : power) {
+        for (DeviceMapping ttdPowerSupply : power) {
             if (ttdPowerSupply != null) {
                 try {
                     mapper.save(ttdPowerSupply);
-                    logger.info("Successfully inserted the data into DB...");
+                    // logger.info("Successfully inserted the data into DB...");
                     isDataInserted = true;
                 } catch (Exception ex) {
                     ex.printStackTrace();
-                    logger.error("Error occur while inserting data..." + ex.getMessage());
+                    // logger.error("Error occur while inserting data..." + ex.getMessage());
                 }
             } else {
-                logger.info("Request Body has some null values...");
+                // logger.info("Request Body has some null values...");
             }
         }
         return isDataInserted;
     }
 
-   /* public List<TTDPowerSupply> getData(SmartTrackRequest smartTrackRequest) {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
-        DynamoDBMapper mapper = new DynamoDBMapper(client);
+    /*
+     * public List<TTDPowerSupply> getData(SmartTrackRequest smartTrackRequest) {
+     * AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
+     * DynamoDBMapper mapper = new DynamoDBMapper(client);
+     *
+     * List<TTDPowerSupply> ttdPowerSupplyList = new ArrayList<>(); try {
+     * ttdPowerSupplyList = getDataOfSpecificDeviceId(mapper, smartTrackRequest); if
+     * (ttdPowerSupplyList != null) { return ttdPowerSupplyList; } else { return
+     * null; } } catch (Exception e) { throw new TTDCustomException(e.getMessage());
+     * }
+     *
+     * }
+     */
 
-        List<TTDPowerSupply> ttdPowerSupplyList = new ArrayList<>();
-        try {
-            ttdPowerSupplyList = getDataOfSpecificDeviceId(mapper, smartTrackRequest);
-            if (ttdPowerSupplyList != null) {
-                return ttdPowerSupplyList;
-            } else {
-                return null;
-            }
-        } catch (Exception e) {
-            throw new TTDCustomException(e.getMessage());
-        }
-
-    }*/
-
-    public List<Voltage> getVoltageData(String tStamp) {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
+    public List<Voltage> getVoltageData(SmartTrackRequest smartTrackRequest) {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials("your_access",
+                "your_secret");
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
+        // AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
         DynamoDBMapper mapper = new DynamoDBMapper(client);
 
         List<Voltage> voltageList = new ArrayList<>();
         try {
-            voltageList = getVoltageList(mapper, tStamp);
+            voltageList = getVoltageList(mapper, smartTrackRequest.getTimestamp(), smartTrackRequest.getDeviceName());
             if (voltageList != null) {
                 return voltageList;
             } else {
@@ -103,8 +107,8 @@ public class SmartTrackImpl {
         DynamoDBMapper mapper = new DynamoDBMapper(client);
         Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
         eav.put(":val1", new AttributeValue().withS(deviceName));
-        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
-                .withFilterExpression("deviceName = :val1").withExpressionAttributeValues(eav);
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression().withFilterExpression("deviceName = :val1")
+                .withExpressionAttributeValues(eav);
         List<TTDPowerSupply> powerSupplyArrayList = mapper.scan(TTDPowerSupply.class, scanExpression);
         return powerSupplyArrayList;
     }
@@ -113,8 +117,8 @@ public class SmartTrackImpl {
         Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
         eav.put(":val1", new AttributeValue().withS(deviceId));
 
-        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
-                .withFilterExpression("deviceId = :val1").withExpressionAttributeValues(eav);
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression().withFilterExpression("deviceId = :val1")
+                .withExpressionAttributeValues(eav);
         List<TTDPowerSupply> ttdPowerSupplyList = mapper.scan(TTDPowerSupply.class, scanExpression);
         List<TTDPowerSupply> getTtdPowerSupplyList = new ArrayList<>();
         for (TTDPowerSupply ttdPowerSupply : ttdPowerSupplyList) {
@@ -131,70 +135,101 @@ public class SmartTrackImpl {
         return getTtdPowerSupplyList;
     }
 
-    private List<Voltage> getVoltageList(DynamoDBMapper mapper, String timestamp) throws Exception {
+    private List<Voltage> getVoltageList(DynamoDBMapper mapper, String timestamp, String deviceName) throws Exception {
+        System.out.println("Entered getVoltageList method to fetch the voltage of devices =============");
         Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
-        eav.put(":tStamp", new AttributeValue().withS(timestamp));
-        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
-                .withFilterExpression("contains(tStamp, :tStamp)").withExpressionAttributeValues(eav);
-        List<TTDPowerSupply> ttdPowerSupplyList = mapper.scan(TTDPowerSupply.class, scanExpression);
         List<TTDPowerSupply> getTtdPowerSupplyList = new ArrayList<>();
         List<Voltage> voltageList = new ArrayList<>();
-        int count = 24;
-        int c = 0;
         Map<String, String> map = new HashMap<>();
+        int count = 1;
+        int c = 0;
+        eav.put(":val1", new AttributeValue().withS(deviceName));
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression().withFilterExpression("deviceName = :val1")
+                .withExpressionAttributeValues(eav);
+        /*eav.put(":tStamp", new AttributeValue().withS(timestamp));
+        //eav.put(":deviceName", new AttributeValue().withS(deviceName));
+        System.out.println("before scan expression call=======================");
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("contains(tStamp, :tStamp)").withLimit(10)
+                //.withFilterExpression("deviceName = :deviceName AND contains(tStamp, :tStamp)")
+                .withExpressionAttributeValues(eav);*/
+
+        List<TTDPowerSupply> ttdPowerSupplyList = mapper.scan(TTDPowerSupply.class, scanExpression);
+        System.out.println("size of devices found==============>"+ttdPowerSupplyList.size());
+        System.out.println("after scan expression call=======================");
         for (TTDPowerSupply ttdPowerSupply : ttdPowerSupplyList) {
-            if (count != 0) {
+            if (ttdPowerSupply.gettStamp().contains(timestamp)) {
                 Gson gson = new Gson();
                 ttdPowerSupply.setPower(ttdPowerSupply.getPower().replaceAll("'", ""));
                 ttdPowerSupply.setEnergy(ttdPowerSupply.getEnergy().replaceAll("'", ""));
                 JsonParser jsonParser = new JsonParser();
                 JsonObject objectFromString = jsonParser.parse(ttdPowerSupply.toString()).getAsJsonObject();
                 ttdPowerSupply = gson.fromJson(objectFromString, TTDPowerSupply.class);
-
-                if (ttdPowerSupply.gettStamp().contains(timestamp)) {
-                    getTtdPowerSupplyList.add(ttdPowerSupply);
-
-                    if (ttdPowerSupply.getPower() != null && ttdPowerSupply.getPower().contains("rphvol")) {
-                        String[] s = ttdPowerSupply.getPower().split(",");
-                        String rPhVol = s[1].replaceAll("rphvol:", "").replace("V", "").trim();
-                        if (count == 24) {
+                getTtdPowerSupplyList.add(ttdPowerSupply);
+                if (ttdPowerSupply.getPower() != null && ttdPowerSupply.getPower().contains("rphvol")) {
+                    String[] s = ttdPowerSupply.getPower().split(",");
+                    String rPhVol = s[1].replaceAll("rphvol:", "").replace("V", "").trim();
+                    if (count == 1) {
+                        map.put(ttdPowerSupply.getDeviceId(), rPhVol);
+                        count = 0;
+                    } else {
+                        if (map.containsKey(ttdPowerSupply.getDeviceId())) {
+                            String s3 = map.get(ttdPowerSupply.getDeviceId());
+                            map.put(ttdPowerSupply.getDeviceId(), s3.concat(", " + rPhVol));
+                        } else if (c == 0) {
                             map.put(ttdPowerSupply.getDeviceId(), rPhVol);
+                            c++;
                         } else {
-                            if (map.containsKey(ttdPowerSupply.getDeviceId())) {
-                                String s3 = map.get(ttdPowerSupply.getDeviceId());
-                                map.put(ttdPowerSupply.getDeviceId(), s3.concat(", " + rPhVol));
-                            } else if (c == 0) {
-                                map.put(ttdPowerSupply.getDeviceId(), rPhVol);
-                                c++;
-                            } else {
-                                String newVol = rPhVol;
-                                map.put(ttdPowerSupply.getDeviceId(), newVol);
-                            }
+                            String newVol = rPhVol;
+                            map.put(ttdPowerSupply.getDeviceId(), newVol);
                         }
-                        count--;
                     }
                 }
-            } else {
-                break;
             }
         }
         for (Map.Entry<String, String> s1 : map.entrySet()) {
             String s4[] = s1.getValue().split(",");
-            List<Double> list = new ArrayList<>();
+            Set<Double> set = new HashSet<>();
             for (String d : s4) {
-                list.add(Double.parseDouble(d.trim()));
+                int finalCount = set.size();
+                if (finalCount <= 23) {
+                    set.add(Double.parseDouble(d.trim()));
+                }else {
+                    break;
+                }
             }
-            Voltage voltage = new Voltage(list, s1.getKey());
+            Voltage voltage = new Voltage(set, s1.getKey());
             voltageList.add(voltage);
         }
+        System.out.println("fetch voltage list functionality completed =======================");
         return voltageList;
     }
 
+    public DeviceName getDeviceNames(String deviceName) {
+        deviceName=deviceName.toLowerCase();
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials("your_access_key",
+                "your_secret_key");
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
+        DynamoDBMapper mapper = new DynamoDBMapper(client);
+        Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+        eav.put(":deviceName", new AttributeValue().withS(deviceName));
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("contains(deviceName, :deviceName)").withExpressionAttributeValues(eav);
+
+        List<DeviceMapping> deviceList = mapper.scan(DeviceMapping.class, scanExpression);
+        DeviceName deviceName1=null;
+        for (DeviceMapping s:deviceList) {
+            String[] str = s.getDeviceName().split(",");
+            deviceName1 = new DeviceName(str);
+        }
+        return deviceName1;
+    }
 }
 
-//Inserting data with table ------------
+// Inserting data with table ------------
 /*
-* DynamoDBTableMapper<SmartTrack,String,?> table = mapper.newTableMapper(SmartTrack.class);
-table.createTableIfNotExists(new ProvisionedThroughput(25L, 25L));
-table.saveIfNotExists(smartTrack);
-* */
+ * DynamoDBTableMapper<SmartTrack,String,?> table =
+ * mapper.newTableMapper(SmartTrack.class); table.createTableIfNotExists(new
+ * ProvisionedThroughput(25L, 25L)); table.saveIfNotExists(smartTrack);
+ */
